20bc82e58143b5a4caea4232315cd8b5678027dc
Erika Szabo <erika.szabo.geo@gmail.com>
2018-10-03 15:22:50 +0000

    just fixed typo errors in database

--- express_server.js
+++ express_server.js
@@ -5,7 +5,7 @@ var PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 var urlDatabase = {
-  "b2xVn2": "http:lighthouselabs.ca",
+  "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
@@ -36,8 +36,19 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  // console.log(req.body);  // debug statement to see POST parameters
+  let shortURL = generateRandonString();
+  urlDatabase[shortURL] = req.body.longURL;    // Respond with 'Ok'
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.get("/u/:shortURL", (req, res, next) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    res.send('No such shortURL!!\n');
+  }
 });
 
 app.listen(PORT, () => {
