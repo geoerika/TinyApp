75b9fdb97d78ac7d1fb0d5eaa2ee8ed00720c8f5
Erika Szabo <erika.szabo.geo@gmail.com>
2018-10-05 22:07:16 +0000

    changed all cookies to session encrypted ones

--- express_server.js
+++ express_server.js
@@ -1,12 +1,18 @@
 var express = require('express');
-var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
+var bcrypt = require('bcryptjs');
+
 
 var app = express();
 
 var PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
-app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: ['hsdklhfd'],
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
 
 const urlDatabase = {
   'b2xVn2': {
@@ -79,7 +85,7 @@ function matchEmailPass (email, password) {
   return foundMatch;
 }
 
-function userIdbyEmail (email) {
+function userIdbyEmail (email) { //gets the user.id if email known
   const usersArray = Object.values(users); //creates a list of users
   let userId = 0;
   for (let user of usersArray) {
@@ -88,6 +94,18 @@ function userIdbyEmail (email) {
     }
   }
   return userId;
+ }
+
+ function userPassbyEmail (email) {  //gets user.password if email known
+  const usersArray = Object.values(users); //creates a list of users
+  let userPass = 0;
+  for (let user of usersArray) {
+    if (user.email === email) {
+      userPass = user.password;
+      console.log("userPassword: ", userPass);
+    }
+  }
+  return userPass;
  }
 
 const urlsForUser = (userId) => { //returns a list of objects with shortURL and longURL as keys
@@ -107,7 +125,7 @@ const urlsForUser = (userId) => { //returns a list of objects with shortURL and 
 
 app.get('/', (req, res) => {
 
-  if (req.cookies.user_id) {
+  if (req.session.user_id) {
     res.redirect('/urls');
   } else {
     res.redirect('/login');
@@ -115,9 +133,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  if (req.cookies.user_id) {
-    let userId = req.cookies.user_id;
+  if (req.session.user_id) {
+    let userId = req.session.user_id;
     let userUrlsArray= urlsForUser(userId, urlDatabase);
+    console.log("urlDatabase:", urlDatabase);
     let templateVars = {
       email: users[userId].email,
       urls: userUrlsArray
@@ -129,7 +148,7 @@ app.get('/urls', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-  let userId = req.cookies.user_id;
+  let userId = req.session.user_id;
   let templateVars = {
     user: users[userId],
     greeting: 'Hello World!'
@@ -139,8 +158,8 @@ app.get('/hello', (req, res) => {
 
 app.get('/urls/new', (req, res) => {
 
-  if(req.cookies.user_id) {
-    let userId = req.cookies.user_id;
+  if(req.session.user_id) {
+    let userId = req.session.user_id;
     let templateVars = {
       user: users[userId],
       email: users[userId].email
@@ -152,7 +171,7 @@ app.get('/urls/new', (req, res) => {
 });
 
 app.get('/urls/:id', (req, res) => {
-  let userId = req.cookies.user_id;
+  let userId = req.session.user_id;
   let templateVars = {
     user: users[userId],
     email: users[userId].email,
@@ -168,7 +187,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.post('/urls', (req, res) => {
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = {};
-  urlDatabase[shortURL].longURL = req.body.longURL;    // Respond with 'Ok'
+  urlDatabase[shortURL].longURL = req.body.longURL;
+  urlDatabase[shortURL].userID = req.session.user_id;    // Respond with 'Ok'
   res.redirect(`/urls/${shortURL}`);
 });
 
@@ -194,7 +214,7 @@ app.get("/u/:shortURL", (req, res, next) => {
 });
 
 app.post('/urls/:id/delete', (req, res) => {
-  if (req.cookies.user_id === urlDatabase[req.params.id].userID) {
+  if (req.session.user_id === urlDatabase[req.params.id].userID) {
     delete urlDatabase[req.params.id];
     res.redirect('/urls');
   } else {
@@ -203,9 +223,10 @@ app.post('/urls/:id/delete', (req, res) => {
 });
 
 app.post('/urls/:id/update', (req, res) => {
-  console.log("req.cookies.user_id", req.cookies.user_id);
-  if (req.cookies.user_id === urlDatabase[req.params.id].userID) {
+  console.log("req.session.user_id", req.session.user_id);
+  if (req.session.user_id === urlDatabase[req.params.id].userID) {
     urlDatabase[req.params.id].longURL = req.body.fullURL;
+    console.log("urlDatabase[req.params.id].longURL:", urlDatabase[req.params.id].longURL);
     res.redirect('/urls');
   } else {
     res.status(403).send('No Permission to Access');
@@ -220,15 +241,16 @@ app.post('/login', (req, res) => {
 
   let userEmail = req.body.email;
   let userPassword = req.body.password;
+  let userPassinDatabase = userPassbyEmail(userEmail);
 
   if (!findMatch(userEmail, users)) {
     res.status(403).send('No Permission to Access');
   } else {
-    if (!matchEmailPass(userEmail, userPassword, users)) {
+    if (!bcrypt.compareSync(userPassword, userPassinDatabase)) {
     res.status(403).send('No Permission to Access');
     } else {
       let userId = userIdbyEmail(userEmail);
-      res.cookie('user_id', userId);
+      req.session.user_id = userId;
       res.redirect('/');
     }
   }
@@ -247,6 +269,7 @@ app.post('/register', (req, res) => {
 
   let userEmail = req.body.email;
   let userPassword = req.body.password;
+  const hashedPassword = bcrypt.hashSync(userPassword, 10);
   if (!userEmail || !userPassword) {
     res.status(400).send('Bad Request');
   } else if (findMatch(userEmail, users)) {
@@ -256,8 +279,8 @@ app.post('/register', (req, res) => {
     users[userId] = {};
     users[userId].id = userId;
     users[userId].email = userEmail;
-    users[userId].password = userPassword;
-    res.cookie('user_id', userId);
+    users[userId].password = hashedPassword;
+    req.session.user_id = userId;
     res.redirect('/urls');
   }
 });
